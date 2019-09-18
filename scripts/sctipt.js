c6931f06d4feacee37abfb0388b558a806c5a720
Andrey Zhukov <zhukov290791@gmail.com>
2019-09-18 05:10:51 +0500

    lesson 13, first commit

--- scripts/sctipt.js
+++ scripts/sctipt.js
@@ -34,5 +34,62 @@ window.addEventListener('DOMContentLoaded', function () {
         //updateClock();
     }
     countTimer('18 september 2019');
+//Меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
 
+
+
+            /*if(!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+                menu.style.transform = `translate(0)`;
+            }
+            else {
+                menu.style.transform = `translate(-100%)`;
+            }*/
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+    };
+
+    toggleMenu();
+
+    const togglePopUp = () => {
+
+
+        let opacity1 = (i) => {
+            console.log(i);
+            if (i < 0.5) {
+                popup.style.opacity = `${i}`;
+                i += 0.1;
+                console.log(opacity1(i));
+                //setTimeout(function () {console.log('ok')}, 3000);
+                opacity1(i);
+            }
+        };
+        const popup = document.querySelector('.popup'),
+
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupClose = document.querySelectorAll('.popup-close');
+
+        popupBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popup.style.opacity = '0';
+                popup.style.display = 'block';
+                opacity1(0);
+            });
+        });
+        popupClose.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'none';
+            });
+        });
+    };
+    togglePopUp();
 });
